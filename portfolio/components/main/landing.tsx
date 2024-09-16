649f7c36a9f0428afa2ba8b499589e2898a76a92
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-09-16 09:19:21 +0000

    working on some important updates

--- portfolio/components/main/landing.tsx
+++ portfolio/components/main/landing.tsx
@@ -3,7 +3,22 @@
 const Landing = () => {
 
     return (
-        <div className="mt-">
+        <div className="mt-20">
+         
+         <div className="bg-primary text-white">
+            <div className="bg-secondary-100 text-wrap">
+            I'm a software engineer, specialized in Backend Engineering with a deep passion for fetching data. With over 4 years of experience, I build scalable, fast, robust, and secured applications using the most up-to-date technologies available. My forte lies in Quality Assurance and Automation – ensuring that projects meet the highest QA standards while automating any repetitive tasks to streamline processes.
+
+When it comes to Tutoring, I find joy in sharing my knowledge with students of all ages. Whether it's explaining complex concepts or simplifying technical jargon, I can easily adjust my teaching approach to suit different age groups. 
+
+I am dedicated to continuous learning to refine my skills and stay relevant to the evolving technological fields
+            </div>
+
+            <div className="bg-primary text-white">
+                image here
+            </div>
+
+         </div>
             
         </div>
     )
