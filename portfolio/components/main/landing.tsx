a017da35f6af2f4638f3c09a0c10b7a91405b20c
Romaric Lonfonyuy <78319422+Romaric250@users.noreply.github.com>
2024-09-16 09:13:22 +0000

    making some new updates

--- portfolio/components/main/landing.tsx
+++ portfolio/components/main/landing.tsx
@@ -3,29 +3,8 @@
 const Landing = () => {
 
     return (
-        <div>
-            lorem200fkdjngdfjg dfj gdfj g
-            dffd
-            gfd
-            df
-            gfd
-            gfd
-            fd
-            df
-            gfd
-            gf
-            fd
-            gkdf
-            gkd
-            fkgf
-            kg
-            fkgf
-            gkf
-            kf
-            f
-            f
-            kfd
-            kd
+        <div className="mt-">
+            
         </div>
     )
 }
